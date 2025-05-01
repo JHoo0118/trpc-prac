10dcf1761696b15d85710838b501a476b922b0ad
JHoo0118 <kik3078@gmail.com>
2025-05-01 20:47:46 +0900

    feat: Experiences

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
 import { useState } from "react";
 
+import { ExperienceList } from "./features/experiences/components/ExperienceList";
 import Navbar from "./features/shared/components/Navbar";
 import { ThemeProvider } from "./features/shared/components/ThemeProvider";
 import { Toaster } from "./features/shared/components/ui/Toaster";
@@ -36,25 +37,23 @@ export function App() {
                 </p>
               </header>
               <div className="space-y-4 p-4">
-                <img
-                  src="/500w-logo.png"
-                  alt="logo"
-                  className="mx-auto h-24 w-24"
-                />
-                <div className="space-y-2">
-                  <h1 className="text-center text-2xl font-semibold">
-                    Welcome to the course!
-                  </h1>
-                  <p className="text-center text-lg text-neutral-500">
-                    You're going to build a lot of great things here. Let's get
-                    started!
-                  </p>
-                </div>
+                <Index />
               </div>
             </div>
           </div>
         </ThemeProvider>
       </QueryClientProvider>
     </trpc.Provider>
+  );
+}
+
+function Index() {
+  const experiencesQuery = trpc.experiences.feed.useQuery({});
+
+  return (
+    <ExperienceList
+      experiences={experiencesQuery.data?.experiences ?? []}
+      isLoading={experiencesQuery.isLoading}
+    />
   );
 }
