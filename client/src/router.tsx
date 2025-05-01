2e7eec5d25156de3ceb93ca795eafb7fb62e4758
JHoo0118 <kik3078@gmail.com>
2025-05-01 22:29:34 +0900

    feat: 오류 컴포넌트 추가

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -7,6 +7,7 @@ import {
   httpBatchLink,
 } from "@trpc/react-query";
 
+import { ErrorComponent } from "./features/shared/components/ErrorComponent";
 import Spinner from "./features/shared/components/ui/Spinner";
 import { env } from "./lib/utils/env";
 import { routeTree } from "./routeTree.gen";
@@ -37,6 +38,7 @@ function createRouter() {
         <Spinner />
       </div>
     ),
+    defaultErrorComponent: ErrorComponent,
     Wrap: function WrapComponent({ children }: { children: React.ReactNode }) {
       return (
         <trpc.Provider client={trpcClient} queryClient={queryClient}>
